1552fa7534c52a71f3cb48547d8ad054755f1232
shungo0222 <kimura.shungo@gmail.com>
2023-05-18 23:12:00 +0900

    Redirect to a sample dashboard page by wallet authentication

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -1,7 +1,22 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import styles from "../../styles/Navbar.module.css";
 
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Navbar() {
+	const { data: session } = useSession()
+	const router = useRouter()
+
+	useEffect(() => {
+		if (session) {
+			router.push(`/dashboard/${session.user.name}`)
+		} else {
+			router.push('/')
+		}
+	}, [session])
+
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>

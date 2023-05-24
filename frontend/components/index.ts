59bacade1cd3c3dadd66281cafd62f8b21e2e107
shungo0222 <kimura.shungo@gmail.com>
2023-05-24 16:15:15 +0900

    Implement HP / moving from "escrow" repo

---
+++ frontend/components/index.ts
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+
+// Aesthetic Imports
+import { Glow } from "./aesthetics";
+
+// HP Imports
+import {
+  IntroSection,
+  Features,
+  Walkthrough,
+  CurrentSystemProblems,
+  Footer
+} from "./hp";
+
+export {
+  IntroSection,
+  Footer,
+  Features,
+  Walkthrough,
+  CurrentSystemProblems,
+  Glow,
+  Navbar
+};
--- frontend/pages/dashboard/[walletAddress].js
+++
@@ -1,7 +0,0 @@
-import { useRouter } from 'next/router';
-
-export default function Dashboard() {
-    const router = useRouter();
-
-    return <h1 className='text-white text-3xl flex justify-center'>Dashboard: {router.query.walletAddress}</h1>;
-}

28bb0c79142a4a7e18c54b392ac900a9c6a0213e
shungo0222 <kimura.shungo@gmail.com>
2023-05-25 21:14:46 +0900

    Create "index.ts" in "interfaces"

---
+++ frontend/interfaces/index.ts
@@ -0,0 +1,19 @@
+import { SectionWrapperPropsInterface } from "./sectionWrapper";
+import { 
+  ProjectDataInterface,
+  ProjectDetailInterface
+} from "./dashboard";
+import { 
+  ProblemsInterface,
+  FeaturesInterface,
+  WalkthroughInterface
+} from "./hp";
+
+export type { 
+  ProjectDataInterface, 
+  ProjectDetailInterface,
+  WalkthroughInterface,
+  ProblemsInterface,
+  FeaturesInterface,
+  SectionWrapperPropsInterface
+}

12a1c5fd2796142fc7185232f75c15e9a4b1fc52
shungo0222 <kimura.shungo@gmail.com>
2023-05-25 21:14:13 +0900

    Create "index.ts" in "interfaces/hp"

---
+++ frontend/interfaces/hp/index.ts
@@ -0,0 +1,9 @@
+import { ProblemsInterface } from "./currentSystemProblems"
+import { FeaturesInterface } from "./features"
+import { WalkthroughInterface } from "./walkthrough"
+
+export type {
+  ProblemsInterface,
+  FeaturesInterface,
+  WalkthroughInterface
+}

e4f1e846b7724eccc38390600988da3f9418f2b3
shungo0222 <kimura.shungo@gmail.com>
2023-05-25 19:56:57 +0900

    Create "index.ts" in "types"

---
+++ frontend/types/index.ts
@@ -0,0 +1,15 @@
+import { 
+  ProjectDetailInterfaceKeysType,
+  LineDataType,
+  LineOptionsType,
+  DoughnutDataType,
+  DoughnutOptionsType
+} from "./dashboard";
+
+export type { 
+  ProjectDetailInterfaceKeysType,
+  LineDataType,
+  LineOptionsType,
+  DoughnutDataType,
+  DoughnutOptionsType
+}

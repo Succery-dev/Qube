21208a848b99a83bea692336ed89517a8b2de4c1
shungo0222 <kimura.shungo@gmail.com>
2023-05-27 15:58:05 +0900

    Create a file for creating a project

---
+++ frontend/pages/createProject.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import type { NextPage } from "next";
+
+const CreateProject: NextPage = () => {
+  return <p className="text-5xl flex items-center justify-center h-screen">create project</p>;
+}
+
+export default CreateProject;

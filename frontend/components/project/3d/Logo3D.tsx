d33cb4a860d4dfb5b20b5dd3099bc1247e3de6d7
shungo0222 <kimura.shungo@gmail.com>
2023-06-02 23:08:22 +0900

    Add "scene" in "Canvas"

--- frontend/components/project/3d/Logo3D.tsx
+++ frontend/components/project/3d/Logo3D.tsx
@@ -31,17 +31,18 @@ const Logo = (): JSX.Element => {
   );
 };
 
-// const scene = new THREE.Scene();
-// const light = new THREE.AmbientLight(0x4d7ee9, 1);
+const scene = new THREE.Scene();
+const light = new THREE.AmbientLight(0x4d7ee9, 1);
+scene.add(light);
 
 // 3D Scene Configurations
 const LogoCanvas = (): JSX.Element => {
   return (
-    <Canvas camera={{ position: [10, 0, 0], fov: 25 }}>
+    <Canvas camera={{ position: [10, 0, 0], fov: 25 }} scene={scene}>
       {/* Lighting */}
       {/* TODO: An error occurs with this below on Vercel. (Issue: #65) */}
       {/* <ambientLight color="#4d7ee9" intensity={1} /> */}
-      <ambientLight color={0x4d7ee9} intensity={1} />
+      {/* <ambientLight color={0x4d7ee9} intensity={1} /> */}
       {/* <ambientLight intensity={1} /> */}
       <directionalLight color="#2563EB" position={[0, 0, 5]} />
       <directionalLight color="#00FFFF" position={[0, 0, -5]} />

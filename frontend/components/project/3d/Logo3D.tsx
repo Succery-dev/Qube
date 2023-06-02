8c12db8cf1cd4491626c15544c36213f1289ae05
shungo0222 <kimura.shungo@gmail.com>
2023-06-02 22:45:56 +0900

    Change the color code "#4d7ee9" -> "0x4d7ee9"

--- frontend/components/project/3d/Logo3D.tsx
+++ frontend/components/project/3d/Logo3D.tsx
@@ -31,6 +31,9 @@ const Logo = (): JSX.Element => {
   );
 };
 
+// const scene = new THREE.Scene();
+// const light = new THREE.AmbientLight(0x4d7ee9, 1);
+
 // 3D Scene Configurations
 const LogoCanvas = (): JSX.Element => {
   return (
@@ -38,6 +41,7 @@ const LogoCanvas = (): JSX.Element => {
       {/* Lighting */}
       {/* TODO: An error occurs with this below on Vercel. (Issue: #65) */}
       {/* <ambientLight color="#4d7ee9" intensity={1} /> */}
+      <ambientLight color={0x4d7ee9} intensity={1} />
       {/* <ambientLight intensity={1} /> */}
       <directionalLight color="#2563EB" position={[0, 0, 5]} />
       <directionalLight color="#00FFFF" position={[0, 0, -5]} />

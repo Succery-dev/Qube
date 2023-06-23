72d447d519ee75486a10c79e479d37b837fe2525
shungo0222 <kimura.shungo@gmail.com>
2023-06-23 17:25:56 +0900

    test

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -48,6 +48,7 @@ export default function Home() {
         <IntroSection />
       </SectionWrapper>
       <p>test</p>
+      <IntroSection />
 
       {/* Why QubePay */}
       <SectionWrapper bgColor="bg-black" glowStyles={[]}>

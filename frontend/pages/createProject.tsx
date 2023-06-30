b76ba4597cd099be5f4a616f972d81c9570a3556
shungo0222 <kimura.shungo@gmail.com>
2023-06-30 16:41:21 +0900

    Add "setShowSubmitModal"

--- frontend/pages/createProject.tsx
+++ frontend/pages/createProject.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import type { NextPage } from "next";
 
 import { ProjectScaffold } from "../components";
 
 const CreateProject: NextPage = () => {
-  return <ProjectScaffold />;
+  const [showSubmitModal, setShowSubmitModal] = useState(false);
+
+  return <ProjectScaffold setShowSubmitModal={setShowSubmitModal}/>;
 }
 
 export default CreateProject;

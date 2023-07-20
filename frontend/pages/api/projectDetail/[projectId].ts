27699ebbf1497806b94f911d81a79c63d9c5d135
shungo0222 <kimura.shungo@gmail.com>
2023-07-20 20:04:09 +0900

    Comment out APIs for firebase

--- frontend/pages/api/projectDetail/[projectId].ts
+++ frontend/pages/api/projectDetail/[projectId].ts
@@ -1,24 +1,24 @@
-import { firebaseApp } from "../../../utils"
-import { getFirestore, doc, getDoc } from "firebase/firestore";
+// import { firebaseApp } from "../../../utils"
+// import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-const db = getFirestore(firebaseApp);
+// const db = getFirestore(firebaseApp);
 
 
-export default async (req: any, res: any) => {
-  try {
-    if (req.method === "GET") {
-      const projectRef = doc(db, "projects", req.query.projectId);
-      const projectSnap = await getDoc(projectRef);
+// export default async (req: any, res: any) => {
+//   try {
+//     if (req.method === "GET") {
+//       const projectRef = doc(db, "projects", req.query.projectId);
+//       const projectSnap = await getDoc(projectRef);
 
-      if (projectSnap.exists()) {
-        const projectData = projectSnap.data();
-        res.status(200).json(projectData);
-      } else {
-        res.status(404).json({ message: "Document not found" });
-      }
-    }
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ message: "Server Error" });
-  }
-}
+//       if (projectSnap.exists()) {
+//         const projectData = projectSnap.data();
+//         res.status(200).json(projectData);
+//       } else {
+//         res.status(404).json({ message: "Document not found" });
+//       }
+//     }
+//   } catch (e) {
+//     console.error(e);
+//     res.status(500).json({ message: "Server Error" });
+//   }
+// }

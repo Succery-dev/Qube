accadaf80d85ce73fba2588ad4571c83c456bcf8
shungo0222 <kimura.shungo@gmail.com>
2023-07-20 18:57:09 +0900

    Fix: firebase settings

--- frontend/utils/firebase.ts
+++ frontend/utils/firebase.ts
@@ -1,38 +1,40 @@
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   // TODO: This below causes an error (Issue #47)
-//   // projectId: process.env.FIREBASE_PROJECT_ID,
-//   projectId: "qubepay-succery",
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // TODO: This below causes an error (Issue #47)
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "qubepay-succery",
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
 
-// // Initialize Firebase
+// Initialize Firebase
 // const firebaseApp = initializeApp(firebaseConfig);
 
 // export { firebaseApp }
 
-import { initializeApp } from "firebase/app";
+// ===========================================================================
+
+// import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBjPHE5ESFSd_GhwFzUaE2mTUyqm9sVedo",
-  authDomain: "qube-development-d6811.firebaseapp.com",
-  projectId: "qube-development-d6811",
-  storageBucket: "qube-development-d6811.appspot.com",
-  messagingSenderId: "308534632877",
-  appId: "1:308534632877:web:23ef644fd3ec5873a70124",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBjPHE5ESFSd_GhwFzUaE2mTUyqm9sVedo",
+//   authDomain: "qube-development-d6811.firebaseapp.com",
+//   projectId: "qube-development-d6811",
+//   storageBucket: "qube-development-d6811.appspot.com",
+//   messagingSenderId: "308534632877",
+//   appId: "1:308534632877:web:23ef644fd3ec5873a70124",
+// };
 
-// Initialize Firebase
+// // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
 export const storage = getStorage(app);

ca523cba50f1765f4f08637ef9531196fc882267
shungo0222 <kimura.shungo@gmail.com>
2023-07-20 18:20:31 +0900

    Fix: Duplicate identifier 'ProjectScaffold'

--- frontend/components/project/index.ts
+++ frontend/components/project/index.ts
@@ -1,12 +1,9 @@
 import ProjectScaffold from "./ProjectScaffold";
 import CreateProjectModal from "./CreateProjectModal";
-
-export { ProjectScaffold, CreateProjectModal };
-
 import Dropbox from "./Dropbox";
 import SubmitProjectModal from "./SubmitProjectModal";
 
 // 3d Imports
 import { LogoCanvas } from "./3d";
 
-export { ProjectScaffold, Dropbox, SubmitProjectModal, LogoCanvas };
+export { ProjectScaffold, Dropbox, SubmitProjectModal, LogoCanvas, CreateProjectModal };

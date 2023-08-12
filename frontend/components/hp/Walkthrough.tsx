4e1c954497945b02a54bc89db4f4b34ef85bb7b1
shungo0222 <kimura.shungo@gmail.com>
2023-08-12 10:41:19 +0900

    Add contents to "How to Use"

--- frontend/components/hp/Walkthrough.tsx
+++ frontend/components/hp/Walkthrough.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 
 // Interfaces Imports
@@ -59,6 +59,8 @@ const DemoVideo = (): JSX.Element => {
 };
 
 const Walkthrough = (): JSX.Element => {
+  const [activeTab, setActiveTab] = useState("enterprise");
+
   return (
     <div id="walkthrough">
       <motion.h1
@@ -70,10 +72,93 @@ const Walkthrough = (): JSX.Element => {
       >
         How to Use
       </motion.h1>
-      <p className="text-3xl mt-7">Qube is super simple to use! No Complexity!</p>
-      <div className="sm:flex flex-row items-center md:gap-16 sm:gap-8 sm:mt-24 mt-16">
-        <DemoVideo />
-        <motion.div
+      <p className="text-3xl mt-7 mb-36">Qube is super simple to use! No Complexity!</p>
+      <div className="sm:flex flex-row items-start md:gap-16 sm:gap-8 sm:mt-24 mt-16">
+
+        {/* For Enterprises */}
+        <div className="flex-1 max-w-4xl mx-auto">
+          <h2 
+            className={`text-5xl font-semibold mb-6 ${activeTab === "enterprise" ? "underline text-white" : "text-gray-500"}`}
+            onClick={() => setActiveTab("enterprise")}
+          >
+            For Enterprises
+          </h2>
+          
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-blue-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              1
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">Connect your wallet</h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-green-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              2
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">Create a project & copy the Link</h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-indigo-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              3
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">Share with the Freelancer</h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-yellow-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              4
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">Send your money to Escrow</h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <h3 className="text-2xl font-semibold">Here u go! Leave the rest to Qube! </h3>
+          </div>
+        </div>
+
+        {/* For Individuals */}
+        <div className="flex-1 max-w-4xl mx-auto">
+          <h2 
+            className={`text-5xl font-semibold mb-6 ${activeTab === "individuals" ? "underline text-white" : "text-gray-500"}`}
+            onClick={() => setActiveTab("individuals")}
+          >
+            For Individuals
+          </h2>
+          
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-blue-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              1
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">Open the link using your wallet</h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <div className="flex-shrink-0 bg-green-500 text-white rounded-full w-16 h-16 flex items-center justify-center mr-4 text-2xl">
+              2
+            </div>
+            <div>
+              <h3 className="text-2xl font-semibold">check the details and sign </h3>
+            </div>
+          </div>
+
+          <div className="flex items-center mb-4">
+            <h3 className="text-2xl font-semibold">Done! The payment will be executed automatically after the work is done!</h3>
+          </div>
+        </div>
+
+        {/* <DemoVideo /> */}
+        {/* <motion.div
           variants={fadeIn("left", 1.25)}
           initial="hidden"
           whileInView="visible"
@@ -94,7 +179,7 @@ const Walkthrough = (): JSX.Element => {
               </motion.div>
             );
           })}
-        </motion.div>
+        </motion.div> */}
       </div>
     </div>
   );

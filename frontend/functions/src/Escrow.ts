70ad8ae41ccda8aa9f0cb9159ce69ae58f21133a
shungo0222 <kimura.shungo@gmail.com>
2023-09-14 16:31:41 +0900

    Create a cloud function for "No Submission By Lancer"

---
+++ frontend/functions/src/Escrow.ts
@@ -0,0 +1,44 @@
+import {ethers, Wallet} from "ethers";
+import EscrowArtifact from "./Escrow.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const LOCAL_NODE_URL = process.env.NODE_URL;
+const jsonRpcProvider = new ethers.JsonRpcProvider(LOCAL_NODE_URL);
+
+const ownerPrivateKey = process.env.SECRET_KEY;
+if (!ownerPrivateKey) {
+  throw new Error("OWNER_PRIVATE_KEY is not defined in .env");
+}
+const ownerWallet = new Wallet(ownerPrivateKey, jsonRpcProvider);
+
+const EscrowAddress = process.env.ESCROW_ADDRESS as string;
+
+// Get escrow contract
+/**
+ * This is for getting an escrow contract
+ *
+ * @param {ethers.Signer | ethers.Provider} signerOrProvider
+ *  - A signer or provider to create a contract instance
+ * @return {ethers.Contract} - An escrow contract instance
+ */
+function getEscrowContract(
+  signerOrProvider: ethers.Signer | ethers.Provider
+): ethers.Contract {
+  return new ethers.Contract(
+    EscrowAddress, EscrowArtifact.abi, signerOrProvider
+  );
+}
+
+/**
+ * Withdraws tokens to the depositor by the owner.
+ *
+ * @param {string} depositId - The ID of the deposit.
+ * @return {Promise<TransactionResponse>}
+ *  A promise that resolves to a transaction response.
+ */
+export async function withdrawTokensToDepositorByOwner(depositId: string) {
+  const contract = getEscrowContract(ownerWallet);
+  const tx = await contract.withdrawTokensToDepositorByOwner(depositId);
+  return await tx.wait();
+}
--- frontend/functions/src/index.ts
+++ frontend/functions/src/index.ts
@@ -1,19 +1,43 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// Import types
+import {App} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import * as functions from "firebase-functions";
+const logger = functions.logger;
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// The Firebase Admin SDK to access Firestore.
+import * as admin from "firebase-admin/app";
+import * as firestoreAdmin from "firebase-admin/firestore";
+import {onSchedule} from "firebase-functions/v2/scheduler";
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import {withdrawTokensToDepositorByOwner} from "./Escrow";
+
+const app: App = admin.initializeApp();
+const firestore: Firestore = firestoreAdmin.getFirestore(app);
+
+export const checkSubmissionDeadline = onSchedule("0 0 * * *", async () => {
+  const now = new Date();
+  // Filter the projects
+  const projects = await firestore
+    .collection("projects")
+    .where("Status", "==", "Waiting for Submission")
+    .where("Deadline(UTC)", "<=", now.toISOString())
+    .get();
+
+  // Return tokens to clients "② No Submission By Lancer"
+  projects.forEach(async (doc) => {
+    // Log the project ID
+    logger.log("② No Submission By Lancer: ", doc.id);
+    // Withdraw tokens to depositor by owner
+    const withdrawResult = await withdrawTokensToDepositorByOwner(doc.id);
+    // Log the result
+    logger.log("Withdraw Result: ", withdrawResult);
+    // Change the status to "Complete (No Submission By Lancer)"
+    await firestore
+      .collection("projects")
+      .doc(doc.id)
+      .set({Status: "Complete (No Submission By Lancer)"}, {merge: true});
+    logger.log("Changed the status 'Complete (No Submission By Lancer)'");
+  });
+});

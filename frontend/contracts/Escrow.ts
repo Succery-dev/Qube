56b67d89b1b6a465a6cbac15b5bcfcf67387b298
shungo0222 <kimura.shungo@gmail.com>
2023-09-15 14:24:50 +0900

    Create helper functions for the Escrow contract

---
+++ frontend/contracts/Escrow.ts
@@ -0,0 +1,30 @@
+import { ethers } from "ethers";
+import { getSigner } from "../utils/ethers";
+import EscrowArtifact from "../../backend/artifacts/contracts/Escrow.sol/Escrow.json";
+
+export const EscrowAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+export function getEscrowContract(signerOrProvider: ethers.Signer | ethers.providers.Provider): ethers.Contract {
+  return new ethers.Contract(EscrowAddress, EscrowArtifact.abi, signerOrProvider);
+}
+
+export async function depositTokens(recipient: string, amount: ethers.BigNumberish, depositId: string) {
+  const signer = getSigner() as ethers.Signer;
+  const contract = getEscrowContract(signer);
+  const tx = await contract.depositTokens(recipient, amount, depositId);
+  return await tx.wait();
+}
+
+export async function withdrawTokensToRecipientByDepositor(depositId: string) {
+  const signer = getSigner() as ethers.Signer;
+  const contract = getEscrowContract(signer);
+  const tx = await contract.withdrawTokensToRecipientByDepositor(depositId);
+  return await tx.wait();
+}
+
+export async function getDepositedAmount() {
+  const signer = getSigner() as ethers.Signer;
+  const contract = getEscrowContract(signer);
+  const address = await signer.getAddress();
+  return await contract.deposits(address).then((amount: ethers.BigNumberish) => ethers.utils.formatEther(amount));
+}

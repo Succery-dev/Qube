42f13b9ed2f6a3b2f913e64828b68af047519219
shungo0222 <kimura.shungo@gmail.com>
2023-09-15 16:17:10 +0900

    Add "await" in front of "getSigner()"

--- frontend/contracts/MockToken.ts
+++ frontend/contracts/MockToken.ts
@@ -4,8 +4,8 @@ import MockTokenArtifact from "../../backend/artifacts/contracts/mocks/MockToken
 
 const MockTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-export function getMockTokenContract(signer: ethers.Signer): ethers.Contract {
-  return new ethers.Contract(MockTokenAddress, MockTokenArtifact.abi, signer);
+export function getMockTokenContract(signerOrProvider: ethers.JsonRpcSigner | ethers.JsonRpcProvider): ethers.Contract {
+  return new ethers.Contract(MockTokenAddress, MockTokenArtifact.abi, signerOrProvider);
 }
 
 export async function balanceOf(account: string) {
@@ -15,27 +15,27 @@ export async function balanceOf(account: string) {
 }
 
 export async function transfer(recipient: string, amount: ethers.BigNumberish) {
-  const signer = getSigner();
+  const signer = await getSigner();
   const contract = getMockTokenContract(signer);
   const tx = await contract.transfer(recipient, amount);
   return await tx.wait();
 }
 
 export async function approve(spender: string, amount: ethers.BigNumberish) {
-  const signer = getSigner();
+  const signer = await getSigner();
   const contract = getMockTokenContract(signer);
   const tx = await contract.approve(spender, amount);
   return await tx.wait();
 }
 
 export async function allowance(owner: string, spender: string) {
-  const signer = getSigner();
+  const signer = await getSigner();
   const contract = getMockTokenContract(signer);
   return await contract.allowance(owner, spender);
 }
 
 export async function mint(to: string, amount: ethers.BigNumberish) {
-  const signer = getSigner();
+  const signer = await getSigner();
   const contract = getMockTokenContract(signer);
   const tx = await contract.mint(to, amount);
   return await tx.wait();

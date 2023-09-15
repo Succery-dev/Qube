e1684a74d3475c826c326fcaecf7083812e317d6
shungo0222 <kimura.shungo@gmail.com>
2023-09-15 16:04:49 +0900

    Fix: `ethers.providers.*` -> `ethers.*` (Ref: https://docs.ethers.org/v6/migrating/#migrate-providers)

--- frontend/utils/ethers.ts
+++ frontend/utils/ethers.ts
@@ -2,29 +2,29 @@ import { ethers } from "ethers";
 
 // JSON RPC Provider
 const LOCAL_NODE_URL = "http://127.0.0.1:8545/";
-const jsonRpcProvider = new ethers.providers.JsonRpcProvider(LOCAL_NODE_URL);
+const jsonRpcProvider = new ethers.JsonRpcProvider(LOCAL_NODE_URL);
 
-export function getJsonRpcProvider(): ethers.providers.JsonRpcProvider {
+export function getJsonRpcProvider(): ethers.JsonRpcProvider {
   return jsonRpcProvider;
 }
 
 // Web3 Provider (e.t. Metamask)
-let web3Provider: ethers.providers.Web3Provider | undefined;
+let web3Provider: ethers.BrowserProvider | undefined;
 
 export function initializeWeb3Provider() {
   if (typeof window !== "undefined" && (window as any).ethereum) {
-    web3Provider = new ethers.providers.Web3Provider((window as any).ethereum);
+    web3Provider = new ethers.BrowserProvider((window as any).ethereum);
   }
 }
 
-export function getWeb3Provider(): ethers.providers.Web3Provider | undefined {
+export function getWeb3Provider(): ethers.BrowserProvider | undefined {
   if (!web3Provider) {
     throw new Error("Web3 provider is not initialized. Please call initializeWeb3Provider() first.");
   }
   return web3Provider;
 }
 
-export function getSigner(): ethers.Signer | undefined {
+export function getSigner() {
   if (!web3Provider) {
     throw new Error("Web3 provider is not initialized. Please call initializeWeb3Provider() first.");
   }

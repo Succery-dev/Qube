54d836e6b87fa32e3245429924bb4c0700848095
shungo0222 <kimura.shungo@gmail.com>
2023-09-29 17:48:26 +0900

    Create `Return-to-dashboard` button
    - In `Create Project` page
    - In `Project Detail` page
    
    Related to #205

--- frontend/components/project/ProjectScaffold.tsx
+++ frontend/components/project/ProjectScaffold.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
 
 // Custom components Imports
 import { Glow } from "../";
@@ -49,6 +51,9 @@ const ProjectScaffold = ({
   const form = context.form;
   const setForm = context.setForm;
 
+  const router = useRouter();
+  const { address } = useAccount();
+
   // const [nftAddressDetails, setnftAddressDetails]: [
   //   nftAddressDetails: NftAddressDetailsInterface,
   //   setnftAddressDetails: React.Dispatch<
@@ -76,6 +81,10 @@ const ProjectScaffold = ({
         bgColor="bg-bg_primary"
         glowStyles={aesthetics.glow.createProjectGlowStyles}
       >
+        {/* Return to Dashboard Button */}
+        <button className="bg-orange-500 mb-7 mr-auto p-3 text-xl rounded-full text-white" onClick={() => {
+          router.push(`http://${window.location.host}/dashboard/${address}`);
+        }}>{`<- Return to Dashboard`}</button>
         {/* Create Project Form */}
         <div className="block lg:flex flex-row items-center gap-16">
           {/* Form */}

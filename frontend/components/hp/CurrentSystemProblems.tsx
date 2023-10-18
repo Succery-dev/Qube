907fa7ec46c80a9423c40194deefe07f8fddd185
shungo0222 <kimura.shungo@gmail.com>
2023-10-18 13:56:26 +0900

    Fix a sentence
    - Complexity in management → Complexity in `M`anagement
    
    Related to #288

--- frontend/components/hp/CurrentSystemProblems.tsx
+++ frontend/components/hp/CurrentSystemProblems.tsx
@@ -87,7 +87,7 @@ const whyUsForClient = [
     description: "Clients often experience delayed delivery or inappropriate deliveries. Qube's Escrow payment and arbitration will free you form these concerns.",
   },
   {
-    title: "Complexity in management",
+    title: "Complexity in Management",
     description: "While clients collaborate with several creators it's quite confusing managing all of those. Qube make it way easier to manage payments to influencers.",
   },
   {

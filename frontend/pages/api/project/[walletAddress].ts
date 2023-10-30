d6984b5e4b1ab25c5646fd44b4fe3d5e37be2599
shungo0222 <kimura.shungo@gmail.com>
2023-10-30 15:43:22 +0900

    Improvement: display projects for both companies and creators
    Related to #355

--- frontend/pages/api/project/[walletAddress].ts
+++ frontend/pages/api/project/[walletAddress].ts
@@ -10,13 +10,30 @@ export default async (req: any, res: any) => {
   try {
     if (req.method === "GET") {
       const { walletAddress } = req.query;
-      const projectsQuery = query(projectsCollection, where("Client's Wallet Address", "==", walletAddress));
-      const projectsSnapshot = await getDocs(projectsQuery);
-      const projects = projectsSnapshot.docs.map((project) => ({
+      
+      // Search for projects where "Client's Wallet Address" is equal to walletAddress
+      const clientQuery = query(projectsCollection, where("Client's Wallet Address", "==", walletAddress));
+      const clientSnapshot = await getDocs(clientQuery);
+      const clientProjects = clientSnapshot.docs.map((project) => ({
         id: project.id,
         ...project.data(),
       }));
-      res.status(200).json(projects);
+
+      // Search for projects where "Lancer's Wallet Address" is equal to walletAddress
+      const lancerQuery = query(projectsCollection, where("Lancer's Wallet Address", "==", walletAddress));
+      const lancerSnapshot = await getDocs(lancerQuery);
+      const lancerProjects = lancerSnapshot.docs.map((project) => ({
+        id: project.id,
+        ...project.data(),
+      }));
+
+      // Combine the results and remove duplicates
+      const allProjects = [...clientProjects, ...lancerProjects];
+      const uniqueProjects = Array.from(new Set(allProjects.map(p => p.id))).map(id => {
+        return allProjects.find(p => p.id === id);
+      });
+
+      res.status(200).json(uniqueProjects);
     }
   } catch (e) {
     res.status(400).end();

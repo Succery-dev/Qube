bb6598964585f10e3c1cfb8da58cbefed372df4e
shungo0222 <kimura.shungo@gmail.com>
2023-10-31 18:05:37 +0900

    Update the autotaks code for ERC2771forwarder
    Related to #347

--- backend/autotasks/relay/index.ts
+++ backend/autotasks/relay/index.ts
@@ -5,42 +5,193 @@ dotenv.config();
 
 const ForwarderAbi = [
   {
-    "inputs": [],
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      }
+    ],
     "stateMutability": "nonpayable",
     "type": "constructor"
   },
   {
     "inputs": [
       {
-        "components": [
-          { "internalType": "address", "name": "from", "type": "address" },
-          { "internalType": "address", "name": "to", "type": "address" },
-          { "internalType": "uint256", "name": "value", "type": "uint256" },
-          { "internalType": "uint256", "name": "gas", "type": "uint256" },
-          { "internalType": "uint256", "name": "nonce", "type": "uint256" },
-          { "internalType": "bytes", "name": "data", "type": "bytes" }
-        ],
-        "internalType": "struct MinimalForwarder.ForwardRequest",
-        "name": "req",
-        "type": "tuple"
-      },
-      { "internalType": "bytes", "name": "signature", "type": "bytes" }
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      }
     ],
-    "name": "execute",
-    "outputs": [
-      { "internalType": "bool", "name": "", "type": "bool" },
-      { "internalType": "bytes", "name": "", "type": "bytes" }
-    ],
-    "stateMutability": "payable",
-    "type": "function"
+    "name": "AddressInsufficientBalance",
+    "type": "error"
   },
   {
     "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" }
+      {
+        "internalType": "uint48",
+        "name": "deadline",
+        "type": "uint48"
+      }
     ],
-    "name": "getNonce",
+    "name": "ERC2771ForwarderExpiredRequest",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "signer",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      }
+    ],
+    "name": "ERC2771ForwarderInvalidSigner",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "requestedValue",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "msgValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "ERC2771ForwarderMismatchedValue",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "target",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "forwarder",
+        "type": "address"
+      }
+    ],
+    "name": "ERC2771UntrustfulTarget",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "FailedInnerCall",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "currentNonce",
+        "type": "uint256"
+      }
+    ],
+    "name": "InvalidAccountNonce",
+    "type": "error"
+  },
+  {
+    "inputs": [],
+    "name": "InvalidShortString",
+    "type": "error"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "str",
+        "type": "string"
+      }
+    ],
+    "name": "StringTooLong",
+    "type": "error"
+  },
+  {
+    "anonymous": false,
+    "inputs": [],
+    "name": "EIP712DomainChanged",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "signer",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "nonce",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "success",
+        "type": "bool"
+      }
+    ],
+    "name": "ExecutedForwardRequest",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "eip712Domain",
     "outputs": [
-      { "internalType": "uint256", "name": "", "type": "uint256" }
+      {
+        "internalType": "bytes1",
+        "name": "fields",
+        "type": "bytes1"
+      },
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "version",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "chainId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "verifyingContract",
+        "type": "address"
+      },
+      {
+        "internalType": "bytes32",
+        "name": "salt",
+        "type": "bytes32"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "extensions",
+        "type": "uint256[]"
+      }
     ],
     "stateMutability": "view",
     "type": "function"
@@ -49,64 +200,223 @@ const ForwarderAbi = [
     "inputs": [
       {
         "components": [
-          { "internalType": "address", "name": "from", "type": "address" },
-          { "internalType": "address", "name": "to", "type": "address" },
-          { "internalType": "uint256", "name": "value", "type": "uint256" },
-          { "internalType": "uint256", "name": "gas", "type": "uint256" },
-          { "internalType": "uint256", "name": "nonce", "type": "uint256" },
-          { "internalType": "bytes", "name": "data", "type": "bytes" }
+          {
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "value",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "gas",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint48",
+            "name": "deadline",
+            "type": "uint48"
+          },
+          {
+            "internalType": "bytes",
+            "name": "data",
+            "type": "bytes"
+          },
+          {
+            "internalType": "bytes",
+            "name": "signature",
+            "type": "bytes"
+          }
         ],
-        "internalType": "struct MinimalForwarder.ForwardRequest",
-        "name": "req",
+        "internalType": "struct ERC2771Forwarder.ForwardRequestData",
+        "name": "request",
         "type": "tuple"
+      }
+    ],
+    "name": "execute",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "value",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "gas",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint48",
+            "name": "deadline",
+            "type": "uint48"
+          },
+          {
+            "internalType": "bytes",
+            "name": "data",
+            "type": "bytes"
+          },
+          {
+            "internalType": "bytes",
+            "name": "signature",
+            "type": "bytes"
+          }
+        ],
+        "internalType": "struct ERC2771Forwarder.ForwardRequestData[]",
+        "name": "requests",
+        "type": "tuple[]"
       },
-      { "internalType": "bytes", "name": "signature", "type": "bytes" }
+      {
+        "internalType": "address payable",
+        "name": "refundReceiver",
+        "type": "address"
+      }
+    ],
+    "name": "executeBatch",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "nonces",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "value",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "gas",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint48",
+            "name": "deadline",
+            "type": "uint48"
+          },
+          {
+            "internalType": "bytes",
+            "name": "data",
+            "type": "bytes"
+          },
+          {
+            "internalType": "bytes",
+            "name": "signature",
+            "type": "bytes"
+          }
+        ],
+        "internalType": "struct ERC2771Forwarder.ForwardRequestData",
+        "name": "request",
+        "type": "tuple"
+      }
     ],
     "name": "verify",
     "outputs": [
-      { "internalType": "bool", "name": "", "type": "bool" }
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
     ],
     "stateMutability": "view",
     "type": "function"
   }
 ];
 
-const ForwarderAddress: string = "0xBb5db3ecb7597d2d7AA708266b09B5083a3Fc0Fe";
-const EscrowAddress: string = "0xde036b34b51123bf34c9E32ca9F21507312a9DE2";
+const ForwarderAddress: string = "0xde267Ae5E70C95Fc4D107388110bC141d2FAfcd9";
+const EscrowAddress: string = "0x8665440E185288Ee1b18391bB12050f42ba8110c";
 
 interface RelayRequest {
+  from: string;
   to: string;
+  value: string;
   gas: string;
+  nonce: string;
+  deadline: string;
+  data: string;
+  signature: string;
 }
 
-async function relay(forwarder: Contract, request: RelayRequest, signature: string, whitelist?: string[]): Promise<ContractTransaction> {
+async function relay(forwarder: Contract, request: RelayRequest, whitelist?: string[]): Promise<ContractTransaction> {
   // Decide if we want to relay this request based on a whitelist
   const accepts = !whitelist || whitelist.includes(request.to);
   if (!accepts) throw new Error(`Rejected request to ${request.to}`);
 
   // Validate request on the forwarder contract
-  const valid: boolean = await forwarder.verify(request, signature);
+  const valid: boolean = await forwarder.verify(request);
   if (!valid) throw new Error("Invalid request");
 
   // Send meta-tx through relayer to the forwarder contract
   const gasLimit = (parseInt(request.gas) + 50000).toString();
-  return await forwarder.execute(request, signature, { gasLimit });
+  return await forwarder.execute(request, { gasLimit });
 }
 
 interface HandlerEvent {
   request: {
-    body: {
-      request: RelayRequest;
-      signature: string;
-    };
+    body: RelayRequest;
   };
 }
 
 async function handler(event: HandlerEvent): Promise<{ txHash: string }> {
   // Parse webhook payload
   if (!event.request || !event.request.body) throw new Error("Missing payload");
-  const { request, signature } = event.request.body;
-  console.log("Relaying", request);
+  const signedRequest = event.request.body;
+  console.log("Relaying", signedRequest);
 
   // Initialize Relayer provider and signer, and forwarder contract
   const credentials = { apiKey: `${process.env.RELAYER_API_KEY}`, apiSecret: `${process.env.RELAYER_API_SECRET}` };
@@ -115,7 +425,7 @@ async function handler(event: HandlerEvent): Promise<{ txHash: string }> {
   const forwarder = new Contract(ForwarderAddress, ForwarderAbi, signer);
 
   // Relay transaction!
-  const tx = await relay(forwarder, request, signature, [EscrowAddress]);
+  const tx = await relay(forwarder, signedRequest, [EscrowAddress]);
   console.log(`Sent meta-tx: ${tx.hash}`);
   return { txHash: tx.hash };
 }

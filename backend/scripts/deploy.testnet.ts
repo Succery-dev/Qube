804db8441358356e1f17bc5b2e80c44bc117e7c1
shungo0222 <kimura.shungo@gmail.com>
2023-11-09 12:58:37 +0900

    Update and add verify feature to deploy script file
    Related to #368

--- backend/scripts/deploy.testnet.ts
+++ backend/scripts/deploy.testnet.ts
@@ -4,12 +4,27 @@ import hre from "hardhat";
 import { writeFileSync } from "fs";
 import { Contract } from "ethers";
 import { ContractNames } from "../contractNames";
-import { MumbaiTokenAddresses } from "../tokenAddresses";
 
 async function deploy(hre: HardhatRuntimeEnvironment, name: string, ...params: any[]): Promise<Contract> {
   const Contract = await hre.ethers.getContractFactory(name);
   const contractInstance = await Contract.deploy(...params);
   return await contractInstance.deployed();
+}
+
+async function verifyContract(hre: HardhatRuntimeEnvironment, contractAddress: string, ...constructorArguments: any[]) {
+  try {
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: constructorArguments,
+    });
+    console.log(`Verified contract ${contractAddress}`);
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log(`Contract ${contractAddress} is already verified`);
+    } else {
+      console.error(`Error verifying contract ${contractAddress}:`, error);
+    }
+  }
 }
 
 async function main() {
@@ -18,7 +33,7 @@ async function main() {
     console.log("Deploying contracts with the account:", deployer.address);
 
     const forwarder = await deploy(hre, ContractNames.ERC2771Forwarder, ContractNames.ERC2771Forwarder);
-    const escrow = await deploy(hre, ContractNames.Escrow, forwarder.address, MumbaiTokenAddresses.USDC, deployer.address);
+    const escrow = await deploy(hre, ContractNames.Escrow, forwarder.address, deployer.address);
 
     writeFileSync("deploy.json", JSON.stringify({
       ERC2771Forwarder: forwarder.address,
@@ -26,6 +41,10 @@ async function main() {
     }, null, 2));
 
     console.log(`${ContractNames.ERC2771Forwarder}: ${forwarder.address}\n${ContractNames.Escrow}: ${escrow.address}`);
+
+    await verifyContract(hre, forwarder.address, ContractNames.ERC2771Forwarder);
+    await verifyContract(hre, escrow.address, forwarder.address, deployer.address);
+
     process.exit(0);
   } catch (error) {
     console.error("Error:", error);

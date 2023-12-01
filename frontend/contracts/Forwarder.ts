347e8ab77639dfc3014f1ec033afeead8f709509
shungo0222 <kimura.shungo@gmail.com>
2023-12-01 15:17:35 +0900

    Fix: importing file for deployed contract addresses
    - before: ../../backend/deploy.`mumbai`.json
    - after: ../../backend/deploy.`polygon`.json
    
    Related to #443

--- frontend/contracts/Forwarder.ts
+++ frontend/contracts/Forwarder.ts
@@ -1,6 +1,6 @@
 import { Contract } from "ethers";
 import { getJsonRpcProvider } from "../utils/ethers";
-import deployedContracts from "../../backend/deploy.mumbai.json";
+import deployedContracts from "../../backend/deploy.polygon.json";
 import ForwarderAbi from "./abi/forwarder.json";
 
 export function getForwarderContract(): Contract {
